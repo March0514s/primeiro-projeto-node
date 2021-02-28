711af50b0de1c7f036931108980f40d6ce60ebb3
Marcus <md.daves@gmail.com>
2021-02-28 04:43:53 -0300

    feat: Configurando Estrutura

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'; 
+
+const app = express();
+
+app.get('/', (request, response) => {
+	return response.json({ message: 'Hello World' });
+});
+
+app.listen(4444, () => {
+	console.log('Server started on port 4444!');
+});
+
+
+
+	
