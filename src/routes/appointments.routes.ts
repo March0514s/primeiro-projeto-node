6af5a24dc868a9105c873b22bb0893408de3ee14
Marcus <md.daves@gmail.com>
2021-03-13 18:41:09 -0300

    feat: Criando repositórios

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,33 +1,25 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual} from 'date-fns';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO } from 'date-fns';
 
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRepository = new AppointmentsRepository();
 const appointmentsRouter = Router();
-
-const appointments = [];
-
-
-const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
  	const { provider, date } = request.body;	
 
 	const parsedDate = startOfHour(parseISO(date));
 
-	const appointment = new Appointment(provider, parsedDate);
-
-	const findAppointmentInSameDate = appointments.find(appointment =>
-		isEqual(parsedDate, appointment.date), 
-	);
-
+	const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+	
 	if(findAppointmentInSameDate) {
 		return response.status(400).json({ message: 'This appointment is already booked' });
 	}
 
-
-	appointments.push(appointment);
+	const appointment = appointmentsRepository.create(provider, parsedDate);
 	
-	return response.json({ message: appointment });
+	return response.json(appointment);
 });
 
 export default appointmentsRouter;
