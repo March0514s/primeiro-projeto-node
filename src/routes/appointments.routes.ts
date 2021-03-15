7b34a71382f9bb7e81f9baa25b775b7789c53c68
Marcus <md.daves@gmail.com>
2021-03-15 07:13:03 -0300

    feat: Listando Agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -6,6 +6,13 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRepository = new AppointmentsRepository();
 const appointmentsRouter = Router();
 
+appointmentsRouter.get('/', (request, response) => {
+	const appointments = appointmentsRepository.all();
+	
+	return response.json(appointments);
+
+});
+
 appointmentsRouter.post('/', (request, response) => {
  	const { provider, date } = request.body;	
 
