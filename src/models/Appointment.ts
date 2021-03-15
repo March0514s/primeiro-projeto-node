a07575193534add90b9b38e95925d96f29e1b1eb
Marcus <md.daves@gmail.com>
2021-03-15 08:01:51 -0300

    feat: Trabalhando com dados

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,5 +1,6 @@
 import { v4 as uuid } from 'uuid';
 
+
 class Appointment {
 	id: string;
 
@@ -7,7 +8,7 @@ class Appointment {
 
 	date: Date;
 
-	constructor(provider: string, date: Date) {
+	constructor({ provider, date}: Omit<Appointment, 'id'> ) {
 		this.id = uuid();	
 		this.provider = provider;
 		this.date = date;	
